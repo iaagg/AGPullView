436a3310855919763b05877421734dd8b627a157
Aleksey Getman <getmanag@gmail.com>
2016-12-10 13:05:45 +0300

    Podfile fixed

--- Example/AGPullView Swift/AGPullView_Example-Bridging-Header.h
+++ Example/AGPullView Swift/AGPullView_Example-Bridging-Header.h
@@ -2,6 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "AGPullViewconfigurator.h"
+#import "AGPullViewConfigurator.h"
 #import "AGPullView.h"
-#import "AGconfiguratorDelegate.h"
+#import "AGConfiguratorDelegate.h"
